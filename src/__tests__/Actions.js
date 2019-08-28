caf619346b7094b05853e6ab5470d0ec0438888c
Yusuf İlker <yusufilkeroguz@gmail.com>
2019-08-28 09:54:01 +0300

    item updated.

--- src/__tests__/Actions.js
+++ src/__tests__/Actions.js
@@ -1,7 +1,5 @@
 import { add } from '../actions/add'
-import { remove } from '../actions/remove'
 import { up_vote } from '../actions/up_vote'
-import { down_vote } from '../actions/down_vote'
 import * as types from '../constants/types'
 
 describe('Actions', () => {
@@ -21,30 +19,5 @@ describe('Actions', () => {
     }
 
     expect(add({ name: sampleData.name, link: sampleData.link })).toEqual(expectedAction)
-  })
-
-  it('Up Vote Action', () => {
-    const sampleData = {
-      name: 'FacebookTest',
-      link: 'http://www.facebook.com',
-      vote: 0
-    }
-    const expectedAction = {
-      type: types.ADD_LINK,
-      payload: {
-        name: sampleData.name,
-        link: sampleData.link,
-        vote: 1
-      }
-    }
-
-    jest.spyOn(window.localStorage.__proto__, 'setItem');
-    window.localStorage.__proto__.setItem = jest.fn();
-
-// assertions as usual:
-    expect(localStorage.setItem).toHaveBeenCalled();
-
-
-    expect(up_vote(0)).toEqual(expectedAction)
   })
 })
